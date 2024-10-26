b4a3534e4c12e2aeeb1a13dccba7128ee9c0aa05
James Billcliffe <jbillcliffe@googlemail.com>
2024-10-26 01:54:35 +0000

    FEAT:Added Login/Signup/Logout links in header

--- renterprise/urls.py
+++ renterprise/urls.py
@@ -22,6 +22,7 @@ Including another URLconf
 
 
 urlpatterns = [
+    path('account/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
