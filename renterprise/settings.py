1ca4ccb84d786da7b394757fa6d7582c1c250046
James Billcliffe <jbillcliffe@googlemail.com>
2024-12-01 04:45:11 +0000

    MAINT: Migration to postgres. Fixtures uploaded

--- renterprise/settings.py
+++ renterprise/settings.py
@@ -161,11 +161,11 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
