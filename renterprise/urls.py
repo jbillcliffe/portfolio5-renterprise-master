ead25904dbfe7cd031da61e56e1f773c9457e56a
James Billcliffe <jbillcliffe@googlemail.com>
2024-12-08 03:55:56 +0000

    FIX:URL Paths for robots and sitemap

--- renterprise/urls.py
+++ renterprise/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 """
 URL configuration for renterprise project.
@@ -28,6 +29,16 @@ urlpatterns = [
     path('items/', include('items.urls')),
     path('orders/', include('orders.urls')),
     path('profiles/', include('profiles.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt", content_type="text/plain")
+    ),
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(
+            template_name="sitemap.xml", content_type="text/plain")
+    )
 ] + static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
